1957ca031114d110034f93951d0553758092b1b7
egorPyra <45798682+egorPyra@users.noreply.github.com>
2024-05-31 17:01:59 +0300

    edit src img

--- components/renaissance.js
+++ components/renaissance.js
@@ -10,7 +10,7 @@ export default function Renaissance() {
             <div className={styles.box}>
                 <p className={styles.text}>В&nbsp;эпоху Возрождения (XV&ndash;XVI&nbsp;вв.) возрос интерес к&nbsp;античной культуре во&nbsp;всех ее&nbsp;проявлениях. Переписывая античные тексты, писцы старались копировать и&nbsp;сам шрифт этих текстов, связывая его с&nbsp;античностью, и&nbsp;даже называли его антиква.</p>
 
-                <img className={styles.imgSerif} src='http://bdstudy.ru/wp-content/uploads/2015/09/img242-11.jpg' />
+                <img className={styles.imgSerif} src='img242-11.jpg' />
             </div>
 
             <img className={styles.imgSerifTT} src='serifRen.png' />
